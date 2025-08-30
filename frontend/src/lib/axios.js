bfc1efa24025d84c9e7000482843655ed61d847c
Kamal Singh <152498786+Kamal-singh-01@users.noreply.github.com>
2025-08-30 23:35:23 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5002/api",
+    baseURL:"https://chatapp-yrso.onrender.com/"
     withCredentials: true,
 })
